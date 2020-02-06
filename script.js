ac201943d57839942316a47b1649998ac896a70f
Jasmine Leslie <jasmineleslie@Jasmines-MacBook-Pro.local>
2020-02-06 14:36:29 -0500

    fourth commit

--- script.js
+++ script.js
@@ -10,17 +10,32 @@ function redButton(){
 document.querySelectorAll(".square")
 let gridSquares= document.querySelectorAll("img:first-child")
 for (let i=0; i<gridSquares.length; i++)
+{
 
-gridSquares[i].addEventListener ("click", changePic)
+    gridSquares[i].addEventListener ("click", changePic)
+}
+
 function changePic (e)
-{console.log (e)
-let firstPic= e.target;
-console.log(firstPic)
-firstPic.style.display= "none"
-let secondPic =firstPic.nextSibling 
-console.log(secondPic)
-secondPic.style.display="inline"
+{
+    let firstPic= e.target;
+
+    firstPic.style.display= "none"
+    let secondPic =firstPic.nextSibling 
+
+    secondPic.style.display="inline"
 }
 
 
 
+const twoPoints= document.querySelectorAll('.grid .Two')
+const onePoint= document.querySelector('.grid .One')
+const scoreBox= document.querySelector('h2')
+let score = 0
+
+twoPoints.forEach(twoPoint => {
+    twoPoint.addEventListener('click', (e) => {
+        console.log(e)
+        score = score + 2
+        scoreBox.textContent= "Score" + " " + score
+})
+})
